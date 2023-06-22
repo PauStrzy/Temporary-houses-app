6fb56295fa6f75ac2660c39281c2792d59b434a2
PaulinaST <paustrzy@gmail.com>
2023-06-22 11:50:41 +0200

    Markers on the map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,20 @@
 // import FetchMap from './FetchMap'
-import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api'
-import { useMemo } from 'react'
+import { GoogleMap, MarkerF, useLoadScript, InfoWindowF } from '@react-google-maps/api'
+import { useMemo, useState } from 'react'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 import './Map.css'
+import { homes } from '../../homes'
 
 const Map = () => {
+	const [activeMarker, setActiveMarker] = useState(null)
+
+	const handleActiveMarker = (marker) => {
+		if (marker === activeMarker) {
+			return
+		}
+		setActiveMarker(marker)
+	}
+
 	const { isLoaded } = useLoadScript({
 		googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
 	})
@@ -15,8 +25,23 @@ const Map = () => {
 			{!isLoaded ? (
 				<LoadingSpinner />
 			) : (
-				<GoogleMap mapContainerClassName='map-container' center={center} zoom={10}>
-					<MarkerF position={center} key={1} name={'Pa'}></MarkerF>
+				<GoogleMap mapContainerClassName='map-container' center={center} zoom={11}>
+					{homes.map(({ id, name, surname, coordinates }) => {
+						return (
+							<MarkerF
+								position={coordinates}
+								key={id}
+								name={name}
+								title={name + ' ' + surname}
+								onClick={() => handleActiveMarker(id)}>
+								{activeMarker === id ? (
+									<InfoWindowF onCloseClick={() => setActiveMarker(null)}>
+										<h3>{name + ' ' + surname}</h3>
+									</InfoWindowF>
+								) : null}
+							</MarkerF>
+						)
+					})}
 				</GoogleMap>
 			)}
 		</div>
